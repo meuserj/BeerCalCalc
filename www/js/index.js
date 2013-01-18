c8a3dfc320dab8774c283ddfedb284ec7fa1ed44
John Meuser <meuserj@laptop.(none)>
2013-01-17 19:58:12 -0500

    Form start

--- www/js/index.js
+++ www/js/index.js
@@ -20,14 +20,14 @@ var app = {
     initialize: function() {
         this.bind();
     },
-    bind: function() {
-        document.addEventListener('deviceready', this.deviceready, false);
-    },
-    deviceready: function() {
-        // This is an event handler function, which means the scope is the event.
-        // So, we must explicitly called `app.report()` instead of `this.report()`.
-        app.report('deviceready');
-    },
+//    bind: function() {
+//        document.addEventListener('deviceready', this.deviceready, false);
+//    },
+//    deviceready: function() {
+//        // This is an event handler function, which means the scope is the event.
+//        // So, we must explicitly called `app.report()` instead of `this.report()`.
+//        app.report('deviceready');
+//    },
     report: function(id) {
         // Report the event in the console
         console.log("Report: " + id);
