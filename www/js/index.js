ba645970ce48c0ee9707d456245c95606949efbc
John Meuser <meuserj@laptop.(none)>
2013-01-18 23:40:21 -0500

    Lotta math

--- www/js/index.js
+++ www/js/index.js
@@ -38,5 +38,57 @@ var app = {
         document.querySelector('#' + id + ' .pending').className += ' hide';
         var completeElem = document.querySelector('#' + id + ' .complete');
         completeElem.className = completeElem.className.split('hide').join('');
+    },
+    calcalc: function(element) {
+        var ogInp = document.getElementById("og");
+        var fgInp = document.getElementById("fg");
+        var abvInp = document.getElementById("abv");
+        var quantInp = document.getElementById("quantity");
+        var calDiv = document.getElementById("calories");
+        if(ogInp.value > 1.5)
+            var brixMode = true;
+        else
+            var brixMode = false;
+
+        if(brixMode)
+        {
+            var osg = (ogInp.value / (258.6-((ogInp.value / 258.2)*227.1))) + 1;
+            var fsg = (fgInp.value / (258.6-((fgInp.value / 258.2)*227.1))) + 1;
+        }
+        else
+        {
+            var osg = ogInp.value;
+            var fsg = fgInp.value;
+        }
+        if(element.id == "fg")
+        {
+            var abvVal = (osg - fsg) * 131.25;
+            abvInp.value = abvVal;
+        }
+        if(element.id == "abv")
+        {
+            var fsgVal = osg - abvInp.value / 131.25;
+            if(brixMode)
+            {
+                var brixVal = (((182.4601 * fsgVal -775.6821) * fsgVal +1262.7794) * fsgVal -669.5622);
+                fgInp.value = brixVal;
+            }
+            else
+                fgInp.value = fsgVal;
+        }
+        if(brixMode)
+        {
+            var brixOG = ogInp.value;
+            var brixFG = ogInp.value;
+        }
+        else
+        {
+            var brixOG = (((182.4601 * osgVal -775.6821) * osgVal +1262.7794) * osgVal -669.5622);
+            var brixFG = (((182.4601 * fsgVal -775.6821) * fsgVal +1262.7794) * fsgVal -669.5622);
+        }
+        var abw = 0.79 * abvInp.value / fsg;
+        var re = (0.1808 * brixOG) + (0.8192 * brixFG)
+        var calPerOz = ((6.9 * abw) + 4 * (re - .1)) * fsg * 3.55 / 12;
+        calDiv.innerText = calPerOz * quantInp.value;
     }
 };
